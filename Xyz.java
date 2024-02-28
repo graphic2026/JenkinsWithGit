e6a963dbbc53b74c7b9a58bac9eed4d2e19699a4
Prashant Kumar <94919954+graphic2026@users.noreply.github.com>
2024-02-28 18:24:05 +0530

    Update Xyz.java from github side

--- Xyz.java
+++ Xyz.java
@@ -1,6 +1,7 @@
 public class Xyz{
        public static void main(String args[]){
               System.out.println("i m xyz class");
+              System.out.println("i made a change from github side");
        }
 
 }
