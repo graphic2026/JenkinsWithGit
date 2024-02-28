62ed337e6fb620960d95aaad9c8a135f95638967
Prashant Kumar <94919954+graphic2026@users.noreply.github.com>
2024-02-28 18:59:52 +0530

    check Jenkins

--- Xyz.java
+++ Xyz.java
@@ -3,7 +3,7 @@ public class Xyz{
               System.out.println("i m xyz class");
               System.out.println("i made a change from github side");
 
-                System.out.println("i made aaign a change from github side");
+                System.out.println("i made asign a change from github side");
        }
 
 }
